22c4b5a2046fc47f73621925a6c9ae9bc3ea28c0
Velavalapalli Harish Sarma <168720942+HarishSarmaTR@users.noreply.github.com>
2025-07-24 15:58:16 +0530

    Update AIReview.py

--- AIReview/AIReview.py
+++ AIReview/AIReview.py
@@ -1,11 +1,10 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # FILE: AIReview.py
 
 """
 Author: Velavalapalli Harish Sarma (velavalapalli.harishsarma@thomsonreuters.com)
 """
-
-﻿#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import tkinter as tk
 from tkinter import messagebox, Toplevel, Label, Button
